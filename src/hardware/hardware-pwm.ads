5d7495893a7305387508755c0d5f2758660f6bf5
Kyle <kyle@tyrfingr.is>
2015-06-08 06:47:18 -0700

    Trying to work out the interrupts bit.

--- src/hardware/hardware-pwm.ads
+++ src/hardware/hardware-pwm.ads
@@ -4,19 +4,19 @@ use  Interfaces;
 
 package Hardware.PWM is
 
+   --  Set Servo_ISR as the TIMER1 COMPA handler. See page 101 of the
+   --  ATmega2560 datasheet for more details.
+   procedure Servo_ISR;
+   pragma Machine_Attribute (Entity => Servo_ISR,
+                             Attribute_Name => "interrupt");
+
+   pragma Export (C, Servo_ISR, MCU.Sig_Timer1_COMPA_String);
    Max_Servos : constant := 4;  --  Board has 4 PWM connectors.
    type Servo_Index is range 1 .. Max_Servos;
 
    procedure Connect (pin : in AVR.Bit_Number; which : out Servo_Index);
    procedure Trim (which : in Servo_Index; trim : in Integer_16);
    procedure Set (which : in Servo_Index; us : in Unsigned_16);
-
-   --  Set Servo_ISR as the TIMER1 COMPA handler. See page 101 of the
-   --  ATmega2560 datasheet for more details.
-   procedure Servo_ISR;
-   pragma Machine_Attribute (Entity => Servo_ISR,
-                             Attribute_Name => "interrupt");
-   pragma Export (C, Servo_ISR, MCU.Sig_Timer1_COMPA_String);
 
 private
    Duty_Cycle      : constant := 20000; --  20ms pulse width
